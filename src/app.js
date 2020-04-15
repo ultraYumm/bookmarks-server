87f05ba1487b02ce8d34b0c76690ca904895a649
Stephanie <Stephanie@Office-iMac.local>
2020-04-14 18:20:56 -0700

    created  server, g server, router and tests

---
+++ src/app.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const app = express()
+const bookmarkRouter = require('./bookmark-router')
+
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    //logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+app.use(bookmarkRouter)
+
+app.get("/", (req, res) =>{
+  res.send ('Hello bookmarks')
+})
+
+app.delete('/bookmarks/:id', (req, res) => {
+  const { id } = req.params;
+
+  const bmIndex = bookmarks.findIndex(b => b.id == id);
+
+  if (bmIndex === -1) {
+    logger.error(`Bookmark with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Not Found');
+  }
+
+  bookmarks.splice(bmIndex, 1);
+
+  logger.info(`Bookmark with id ${id} deleted.`);
+  res
+    .status(204)
+    .end();
+});
+
+
+
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app
